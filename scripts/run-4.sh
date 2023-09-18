323928ca4fed24474473b54ad620b6fe774ea232
Yanlei Zhang <yanlei.zhang@mila.quebec>
2023-09-18 16:28:18 -0400

    scripts and models

---
+++ scripts/run-4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+#SBATCH --job-name=1-torus-ratio4
+#SBATCH --output=%j.out
+
+module load miniconda/3
+conda activate sheafnn
+
+python src/torus-1000.py
